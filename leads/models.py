682c4caa3d3c557aafc4456490a5e1c6b9f8782f
Patryk <patrykprause@gmail.com>
2022-03-14 16:09:48 +0100

    added custom user model, Agent and Lead

--- leads/models.py
+++ leads/models.py
@@ -1,21 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+class User(AbstractUser):
+    pass
+
+
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('YouTube', 'YouTube'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    )
-
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
